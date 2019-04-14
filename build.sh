f1a2c478a6fae6f1278fdb37bce92783fe718a7e
Piotr Dlugosz <pidlug@gmail.com>
2019-04-14 14:11:03 +0200

    Add missing cmake module.

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
-mkdir -p build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+#!/bin/sh
+
+# W tym katalogu umieścimy źródła indi-dreamfocuser'a
+D=~/src/dreamfocuser
+[ -d "$D" ] || mkdir -p "$D"
+cd "$D" || { echo "Nie udało się stworzyć środowiska kompilacji dla DreamFocusera!"; exit 1; }
+
+echo "Potrzebujemy git i zależności budowania dla INDI"
+sudo apt install git
+sudo apt build-dep libindi
+
+git clone https://github.com/pidlug/indi-dreamfocuser.git
+cd indi-dreamfocuser
+git pull
+cd ..
+
+[ -d build ] && rm -R build
+mkdir build
+
+cd build && \
+cmake ../indi-dreamfocuser && \
 make && sudo make install
